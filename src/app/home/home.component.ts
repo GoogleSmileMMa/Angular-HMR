264a1eb43166a35e0df4f9574b0d0b0702bdb9a3
myc <mycapple@163.com>
2018-11-27 15:40:40 +0800

    首页功能

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+//导入服务
+import { HomeService } from './home.service'
+
+//导入路由服务
+import { Router } from '@angular/router'
+
+//导入消息提示服务
+import { NzMessageService } from 'ng-zorro-antd'
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  constructor(private homeService: HomeService, private router: Router, private nzMsgService: NzMessageService ) { }
 
-  constructor() { }
-    isCollapsed = false;
+    isCollapsed = false
+
+    //退出
+    logout() {
+      // e.preventDefault()
+      this.homeService.logout().subscribe(
+        res =>{
+          // console.log('success',res)
+          //先清除本地的token
+          localStorage.removeItem('angular-token')
+          //再跳转到登录页
+          this.router.navigate(['/login'])
+        },
+        err =>{
+          console.log('error',err)
+          this.nzMsgService.create('warning', `好像出错了，请稍后再试`)
+        }
+      )
+    }
+  
 
   ngOnInit() {
   }
---
+++ src/app/home/home.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+//导入HttpClient
+import { HttpClient } from '@angular/common/http'
+
+//导入URL
+import { URL } from '../config'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+
+  constructor(private http: HttpClient) { }
+
+  //退出
+  logout() {
+    const token = localStorage.getItem('angular-token')
+
+    return this.http.delete(`${URL}/tokens`,{
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+  }
+}
